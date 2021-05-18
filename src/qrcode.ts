5caad7222a34e6349495ddc813739068e5453186
hankei6km <gh-hankei6km@outlook.jp>
2021-05-18 13:43:38 +0900

    Make byImage()

--- src/qrcode.ts
+++ src/qrcode.ts
@@ -1,39 +1,46 @@
 import * as path from 'path';
-import { Node as MdNode } from 'unist';
-//const QRCode = require('qrcode');
+import { Root, Image } from 'mdast';
 import QRCode from 'qrcode';
 
-const dummyQrcodeFile = 'mdast-qrcode'
+const dummyQrcodeFile = 'mdast-qrcode';
 const qrcodeInAlt = /(^|(^.*):)qrcode:(.+)$/;
 
-export async function toImageDataURL(
-  tree: MdNode,
+export async function byImage(
+  image: Image,
   options?: QRCode.QRCodeToDataURLOptions
-): Promise<MdNode> {
-  if (tree.type === 'root' && Array.isArray(tree.children)) {
+) {
+  const url: string = image.url || '';
+  const alt: string = image.alt || '';
+  if (url.startsWith('qrcode:')) {
+    // as scheme
+    const text = url.slice(7); // 'qrcode:'.length = 7
+    const d = await QRCode.toDataURL(text, options);
+    image.url = d;
+  } else if (path.parse(url).name === dummyQrcodeFile) {
+    // as alt
+    const m = alt.match(qrcodeInAlt);
+    if (m && m[3]) {
+      const d = await QRCode.toDataURL(m[3], options);
+      image.alt = m[2] || '';
+      image.url = d;
+    }
+  }
+}
+
+export async function toImageDataURL(
+  tree: Root,
+  options?: QRCode.QRCodeToDataURLOptions
+): Promise<Root> {
+  if (tree.type === 'root') {
     const l = tree.children.length;
     for (let i = 0; i < l; i++) {
       const c = tree.children[i];
-      if (c.type === 'paragraph' && Array.isArray(c.children)) {
+      if (c.type === 'paragraph') {
         const ll = c.children.length;
         for (let ii = 0; ii < ll; ii++) {
           const cc = c.children[ii];
-          //console.log(cc.type);
           if (cc.type === 'image') {
-            if (cc.url.startsWith('qrcode:')) {
-              // as scheme
-              const text = cc.url.slice(7); // 'qrcode:'.length = 7
-              const d = await QRCode.toDataURL(text, options);
-              cc.url = d;
-            } else if (path.parse(cc.url).name === dummyQrcodeFile) {
-              // as alt
-              const m = cc.alt.match(qrcodeInAlt);
-              if (m && m[3]) {
-                const d = await QRCode.toDataURL(m[3], options);
-                cc.alt = m[2] || '';
-                cc.url = d;
-              }
-            }
+            await byImage(cc, options);
           }
         }
       }
